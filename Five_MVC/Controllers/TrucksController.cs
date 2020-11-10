c5c1b8c141343e2a8ec1be8f6624b08dfb2e2f8f
杰 <@JIE>
2020-11-10 08:56:13 +0800

    asda

---
+++ Five_MVC/Controllers/TrucksController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Five_MVC.Controllers
+{
+    public class TrucksController : Controller
+    {
+        // GET: Trucks
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
