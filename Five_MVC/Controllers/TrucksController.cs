a09585dc2dd58b346ebeb9d4fd810d76d43d632f
杰 <@JIE>
2020-11-10 11:07:50 +0800

    shi

--- Five_MVC/Controllers/TrucksController.cs
+++ Five_MVC/Controllers/TrucksController.cs
@@ -8,10 +8,35 @@ namespace Five_MVC.Controllers
 {
     public class TrucksController : Controller
     {
-        // GET: Trucks
-        public ActionResult Index()
-        {
-            return View();
-        }
+        //// 货车显示
+        //public ActionResult Index()
+        //{
+        //    string sql = $"select * from Trucks";
+        //    List<Trucks> trucks = DAL.DBHelper.GetToList<Trucks>(sql);
+        //    return View(trucks);
+            
+        //}
+
+        ////货车添加
+        //public ActionResult Add(Trucks t)
+        //{
+        //    string sql = $"insert into Trucks(TBrand,TType,TPlate,TQuota,TConsum) values('江淮','大型货车','粤B70CZ9','15','0.13')";
+        //    DAL.DBHelper.ExecuteNonQuery(sql);
+        //    return RedirectToAction("Index");
+        //}
+
+        ////货车删除
+        //public ActionResult Delete()
+        //{
+        //    return View();
+        //}
+
+        ////货车修改
+        //public ActionResult Edit()
+        //{
+        //    return View();
+        //}
+
+
     }
 }
